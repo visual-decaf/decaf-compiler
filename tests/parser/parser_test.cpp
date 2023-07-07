053f156d2dc96b0455d57fad5c2ec0c0510fb837
Gorun <307532500@qq.com>
2023-07-08 00:37:55 +0800

    添加乘除法测试

--- tests/parser/parser_test.cpp
+++ tests/parser/parser_test.cpp
@@ -71,3 +71,51 @@ TEST_CASE("parser_plus_multiply_precedence", "[parser]") {
 
     REQUIRE(expect->equals(result));
 }
+
+TEST_CASE("parser_plus_minus", "[parser]") {
+    token_stream tokenStream{
+            {token_type ::INTEGER, "1"},
+            {token_type ::PLUS, "+"},
+            {token_type ::INTEGER, "2"},
+            {token_type ::MINUS, "-"},
+            {token_type ::INTEGER, "3"},
+            {token_type ::EOL}};
+
+    decaf::Parser parser{tokenStream};
+    parser.parse();
+
+    auto result = parser.get_ast();
+    auto expect = new ast::ArithmeticBinary(
+            new ast::ArithmeticBinary(
+                    new ast::IntConstant(1),
+                    ast::ArithmeticBinary::Operation::PLUS,
+                    new ast::IntConstant(2)),
+            ast::ArithmeticBinary::Operation::MINUS,
+            new ast::IntConstant(3));
+
+    REQUIRE(expect->equals(result));
+}
+
+TEST_CASE("parser_multiply_divide", "[parser]") {
+    token_stream tokenStream{
+            {token_type ::INTEGER, "1"},
+            {token_type ::STAR, "*"},
+            {token_type ::INTEGER, "2"},
+            {token_type ::SLASH, "/"},
+            {token_type ::INTEGER, "3"},
+            {token_type ::EOL}};
+
+    decaf::Parser parser{tokenStream};
+    parser.parse();
+
+    auto result = parser.get_ast();
+    auto expect = new ast::ArithmeticBinary(
+            new ast::ArithmeticBinary(
+                    new ast::IntConstant(1),
+                    ast::ArithmeticBinary::Operation::MULTIPLY,
+                    new ast::IntConstant(2)),
+            ast::ArithmeticBinary::Operation::DIVIDE,
+            new ast::IntConstant(3));
+
+    REQUIRE(expect->equals(result));
+}
