3bfdff66ffcfa727349198285c7462e1b6c329b0
Gorun <307532500@qq.com>
2023-07-07 14:07:27 +0800

    添加ast抽象语法树的表达式部分

---
+++ ast/expr.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "token.h"
+#include <any>
+#include <memory>
+
+namespace decaf::ast {
+struct ArithmeticBinary;
+struct IntConstant;
+} // namespace decaf::ast
+
+namespace decaf {
+struct ExprVisitor {
+    virtual std::any visitArithmeticBinary(std::shared_ptr<ast::ArithmeticBinary>) = 0;
+    virtual std::any visitIntConstant(std::shared_ptr<ast::IntConstant>) = 0;
+};
+} // namespace decaf
+
+namespace decaf::ast {
+struct Expr {
+    virtual std::any accept(ExprVisitor&) = 0;
+};
+
+struct ArithmeticBinary: Expr, public std::enable_shared_from_this<ArithmeticBinary> {
+    std::shared_ptr<Expr> left;
+    decaf::token op;
+    std::shared_ptr<Expr> right;
+
+    std::any accept(ExprVisitor& visitor) override {
+        visitor.visitArithmeticBinary(shared_from_this());
+    }
+};
+
+struct IntConstant: Expr, public std::enable_shared_from_this<IntConstant> {
+    int value;
+
+    std::any accept(ExprVisitor& visitor) override {
+        visitor.visitIntConstant(shared_from_this());
+    }
+};
+} // namespace decaf::ast
