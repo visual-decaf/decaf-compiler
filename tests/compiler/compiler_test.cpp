b8070f69751a43117eae617d10502564b9cd653b
Gorun <307532500@qq.com>
2023-07-08 14:09:39 +0800

    添加编译器单元测试

--- tests/compiler/compiler_test.cpp
+++ tests/compiler/compiler_test.cpp
@@ -20,7 +20,133 @@ TEST_CASE("compiler_main", "[compiler]") {
                     1,
                     Instruction ::GET_INSTANT,
                     2,
-                    Instruction ::PLUS}};
+                    Instruction ::PLUS,
+            }};
 
     REQUIRE(expect == result);
+    delete input_ast;
+}
+
+TEST_CASE("compiler_plus_deep", "[compiler]") {
+    using namespace decaf;
+    auto input_ast = new ast::ArithmeticBinary(
+            new ast::ArithmeticBinary(
+                    new ast::IntConstant(1),
+                    ast::ArithmeticBinary::Operation::PLUS,
+                    new ast::IntConstant(2)),
+            ast::ArithmeticBinary::Operation::PLUS,
+            new ast::IntConstant(3));
+
+    decaf::Compiler compiler{input_ast};
+    compiler.compile();
+
+    using Instruction = ByteCode::Instruction;
+    auto result = compiler.get_program();
+    auto expect = Program{
+            ByteCode{
+                    Instruction ::GET_INSTANT,
+                    1,
+                    Instruction ::GET_INSTANT,
+                    2,
+                    Instruction ::PLUS,
+                    Instruction ::GET_INSTANT,
+                    3,
+                    Instruction ::PLUS,
+            }};
+
+    REQUIRE(expect == result);
+    delete input_ast;
+}
+
+TEST_CASE("compiler_plus_multiply", "[compiler]") {
+    using namespace decaf;
+    auto input_ast = new ast::ArithmeticBinary(
+            new ast::IntConstant(1),
+            ast::ArithmeticBinary::Operation::PLUS,
+            new ast::ArithmeticBinary(
+                    new ast::IntConstant(2),
+                    ast::ArithmeticBinary::Operation::MULTIPLY,
+                    new ast::IntConstant(3)));
+
+    decaf::Compiler compiler{input_ast};
+    compiler.compile();
+
+    using Instruction = ByteCode::Instruction;
+    auto result = compiler.get_program();
+    auto expect = Program{
+            ByteCode{
+                    Instruction ::GET_INSTANT,
+                    1,
+                    Instruction ::GET_INSTANT,
+                    2,
+                    Instruction ::GET_INSTANT,
+                    3,
+                    Instruction ::MULTIPLY,
+                    Instruction ::PLUS,
+            }};
+
+    REQUIRE(expect == result);
+    delete input_ast;
+}
+
+TEST_CASE("compiler_plus_minus", "[compiler]") {
+    using namespace decaf;
+    auto input_ast = new ast::ArithmeticBinary(
+            new ast::ArithmeticBinary(
+                    new ast::IntConstant(1),
+                    ast::ArithmeticBinary::Operation::PLUS,
+                    new ast::IntConstant(2)),
+            ast::ArithmeticBinary::Operation::MINUS,
+            new ast::IntConstant(3));
+
+    decaf::Compiler compiler{input_ast};
+    compiler.compile();
+
+    using Instruction = ByteCode::Instruction;
+    auto result = compiler.get_program();
+    auto expect = Program{
+            ByteCode{
+                    Instruction ::GET_INSTANT,
+                    1,
+                    Instruction ::GET_INSTANT,
+                    2,
+                    Instruction ::PLUS,
+                    Instruction ::GET_INSTANT,
+                    3,
+                    Instruction ::MINUS,
+            }};
+
+    REQUIRE(expect == result);
+    delete input_ast;
+}
+
+TEST_CASE("parser_multiply_divide", "[parser]") {
+    using namespace decaf;
+    auto input_ast = new ast::ArithmeticBinary(
+            new ast::ArithmeticBinary(
+                    new ast::IntConstant(1),
+                    ast::ArithmeticBinary::Operation::MULTIPLY,
+                    new ast::IntConstant(2)),
+            ast::ArithmeticBinary::Operation::DIVIDE,
+            new ast::IntConstant(3));
+
+    decaf::Compiler compiler{input_ast};
+    compiler.compile();
+
+    using Instruction = ByteCode::Instruction;
+    auto result = compiler.get_program();
+    auto expect = Program{
+            ByteCode{
+                    Instruction ::GET_INSTANT,
+                    1,
+                    Instruction ::GET_INSTANT,
+                    2,
+                    Instruction ::MULTIPLY,
+                    Instruction ::GET_INSTANT,
+                    3,
+                    Instruction ::DIVIDE,
+            }};
+
+    REQUIRE(expect == result);
+    delete input_ast;
 }
