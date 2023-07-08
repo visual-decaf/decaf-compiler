acda12478bf571d4b6f3f73365a7cf263aabbb8b
Gorun <307532500@qq.com>
2023-07-08 23:26:16 +0800

    添加decaf基本CLI

---
+++ cli/decaf.cpp
@@ -0,0 +1,60 @@
+#include "compiler.h"
+#include "parser.h"
+#include "scanner.h"
+#include "vm.h"
+
+#include <iostream>
+
+void run_repl() {
+    while (true) {
+        std::cout << ">> ";
+        decaf::Scanner scanner{std::cin};
+        scanner.scan_line();
+        auto token_stream = scanner.get_tokens();
+        token_stream.emplace_back(decaf::token_type::YYEOF);
+
+        decaf::Parser parser{token_stream};
+        parser.parse();
+        auto ast_root = parser.get_ast();
+        if (ast_root == nullptr) {
+            return;
+        }
+
+        decaf::Compiler compiler{ast_root};
+        compiler.compile();
+        auto program = compiler.get_program();
+        delete ast_root;
+
+        decaf::VirtualMachine vm{program};
+        vm.run();
+        std::cout << std::any_cast<int>(vm.top()) << std::endl;
+    }
+}
+
+void test() {
+    decaf::token_stream token_stream;
+    token_stream.emplace_back(decaf::token_type::YYEOF);
+
+    decaf::Parser parser{token_stream};
+    parser.parse();
+    auto ast_root = parser.get_ast();
+    if (ast_root == nullptr) {
+        return;
+    }
+
+    decaf::Compiler compiler{ast_root};
+    compiler.compile();
+    auto program = compiler.get_program();
+    delete ast_root;
+
+    decaf::VirtualMachine vm{program};
+    vm.run();
+    std::cout << std::any_cast<int>(vm.top()) << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    std::cout << "Visual Decaf 0.0.1" << std::endl;
+    run_repl();
+    // test();
+    return 0;
+}
