86993cc289ac410c7adb747d4aeccfc989df5b2e
Gorun <307532500@qq.com>
2023-07-08 16:24:53 +0800

    添加虚拟机单元测试

--- tests/vm/vm_test.cpp
+++ tests/vm/vm_test.cpp
@@ -22,3 +22,87 @@ TEST_CASE("vm_main", "[vm]") {
 
     REQUIRE(std::any_cast<int>(vm.top()) == 6);
 }
+
+TEST_CASE("vm_plus_deep", "[vm]") {
+    using namespace decaf;
+    using Instruction = ByteCode::Instruction;
+    auto input_prog = Program{
+            ByteCode{
+                    Instruction ::GET_INSTANT,
+                    1,
+                    Instruction ::GET_INSTANT,
+                    2,
+                    Instruction ::PLUS,
+                    Instruction ::GET_INSTANT,
+                    3,
+                    Instruction ::PLUS,
+            }};
+
+    decaf::VirtualMachine vm{input_prog};
+    vm.run();
+
+    REQUIRE(std::any_cast<int>(vm.top()) == 6);
+}
+
+TEST_CASE("vm_plus_multiply", "[vm]") {
+    using namespace decaf;
+    using Instruction = ByteCode::Instruction;
+    auto input_prog = Program{
+            ByteCode{
+                    Instruction ::GET_INSTANT,
+                    1,
+                    Instruction ::GET_INSTANT,
+                    2,
+                    Instruction ::GET_INSTANT,
+                    3,
+                    Instruction ::MULTIPLY,
+                    Instruction ::PLUS,
+            }};
+
+    decaf::VirtualMachine vm{input_prog};
+    vm.run();
+
+    REQUIRE(std::any_cast<int>(vm.top()) == 7);
+}
+
+TEST_CASE("vm_plus_minus", "[vm]") {
+    using namespace decaf;
+    using Instruction = ByteCode::Instruction;
+    auto input_prog = Program{
+            ByteCode{
+                    Instruction ::GET_INSTANT,
+                    1,
+                    Instruction ::GET_INSTANT,
+                    2,
+                    Instruction ::PLUS,
+                    Instruction ::GET_INSTANT,
+                    3,
+                    Instruction ::MINUS,
+            }};
+
+    decaf::VirtualMachine vm{input_prog};
+    vm.run();
+
+    REQUIRE(std::any_cast<int>(vm.top()) == 0);
+}
+
+TEST_CASE("vm_multiply_divide", "[vm]") {
+    using namespace decaf;
+    using Instruction = ByteCode::Instruction;
+    auto input_prog = Program{
+            ByteCode{
+                    Instruction ::GET_INSTANT,
+                    1,
+                    Instruction ::GET_INSTANT,
+                    2,
+                    Instruction ::MULTIPLY,
+                    Instruction ::GET_INSTANT,
+                    3,
+                    Instruction ::DIVIDE,
+            }};
+
+    decaf::VirtualMachine vm{input_prog};
+    vm.run();
+
+    REQUIRE(std::any_cast<int>(vm.top()) == 0);
+}
