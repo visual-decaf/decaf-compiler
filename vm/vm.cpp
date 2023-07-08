e4032591b0461678698c0e977dada76ed77e79fd
Gorun <307532500@qq.com>
2023-07-08 16:14:06 +0800

    实现虚拟机 | 使虚拟机访问Program的全部内容

--- vm/vm.cpp
+++ vm/vm.cpp
@@ -1,1 +1,47 @@
 #include "vm.h"
+#include "byte_code_driver.h"
+
+std::any decaf::VirtualMachine::top() {
+    return stk.top();
+}
+
+void decaf::VirtualMachine::op_PLUS() {
+    int rhs = std::any_cast<int>(stk.top());
+    stk.pop();
+    int lhs = std::any_cast<int>(stk.top());
+    stk.pop();
+    stk.emplace(lhs + rhs);
+}
+
+void decaf::VirtualMachine::op_MINUS() {
+    int rhs = std::any_cast<int>(stk.top());
+    stk.pop();
+    int lhs = std::any_cast<int>(stk.top());
+    stk.pop();
+    stk.emplace(lhs - rhs);
+}
+
+void decaf::VirtualMachine::op_MULTIPLY() {
+    int rhs = std::any_cast<int>(stk.top());
+    stk.pop();
+    int lhs = std::any_cast<int>(stk.top());
+    stk.pop();
+    stk.emplace(lhs * rhs);
+}
+
+void decaf::VirtualMachine::op_DIVIDE() {
+    int rhs = std::any_cast<int>(stk.top());
+    stk.pop();
+    int lhs = std::any_cast<int>(stk.top());
+    stk.pop();
+    stk.emplace(lhs / rhs);
+}
+
+void decaf::VirtualMachine::op_GET_INSTANT(uint8_t instant) {
+    stk.emplace(static_cast<int>(instant));
+}
+
+void decaf::VirtualMachine::run() {
+    ByteCodeDriver driver{prog.code, *this};
+    driver.produce();
+}
