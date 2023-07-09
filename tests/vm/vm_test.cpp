e5016867a0bfe5b2c64b8bd40ceeaa2888d86ffe
Gorun <307532500@qq.com>
2023-07-09 16:21:56 +0800

    添加各组件单元测试

--- tests/vm/vm_test.cpp
+++ tests/vm/vm_test.cpp
@@ -106,3 +106,24 @@ TEST_CASE("vm_multiply_divide", "[vm]") {
 
     REQUIRE(std::any_cast<int>(vm.top()) == 0);
 }
+
+TEST_CASE("vm_mod", "[vm]") {
+    using namespace decaf;
+    using Instruction = ByteCode::Instruction;
+    auto input_prog = Program{
+        ByteCode{
+            Instruction ::GET_INSTANT,
+            1,
+            Instruction ::GET_INSTANT,
+            2,
+            Instruction ::MULTIPLY,
+            Instruction ::GET_INSTANT,
+            3,
+            Instruction ::DIVIDE,
+        }};
+
+    decaf::VirtualMachine vm{input_prog};
+    vm.run();
+
+    REQUIRE(std::any_cast<int>(vm.top()) == 0);
+}
