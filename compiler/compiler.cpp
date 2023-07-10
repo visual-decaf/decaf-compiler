a0451c295c901c7796dff8a187dd16d14f1a6dcc
Gorun <307532500@qq.com>
2023-07-10 23:34:37 +0800

    使用智能指针存储AST语法树

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -3,7 +3,7 @@
 #include <stdexcept>
 #include <unordered_map>
 
-std::any decaf::Compiler::visitArithmeticBinary(decaf::ast::ArithmeticBinary* binary) {
+std::any decaf::Compiler::visitArithmeticBinary(std::shared_ptr<decaf::ast::ArithmeticBinary> binary) {
     // Push lhs and rhs first into the stack
     binary->left->accept(*this);
     binary->right->accept(*this);
@@ -24,7 +24,7 @@ std::any decaf::Compiler::visitArithmeticBinary(decaf::ast::ArithmeticBinary* bi
     return {};
 }
 
-std::any decaf::Compiler::visitIntConstant(decaf::ast::IntConstant* constant) {
+std::any decaf::Compiler::visitIntConstant(std::shared_ptr<decaf::ast::IntConstant> constant) {
     if (constant->value > UINT8_MAX) {
         IntConstantPool::index_type index = prog.add_int_constant(constant->value);
         prog.emit_bytes(
@@ -38,7 +38,7 @@ std::any decaf::Compiler::visitIntConstant(decaf::ast::IntConstant* constant) {
     return {};
 }
 
-std::any decaf::Compiler::visitGroup(ast::Group* group) {
+std::any decaf::Compiler::visitGroup(std::shared_ptr<ast::Group> group) {
     group->content->accept(*this);
     return {};
 }
--- compiler/compiler.h
+++ compiler/compiler.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include "byte_code.h"
 #include "expr.h"
 #include "program.h"
@@ -10,19 +12,19 @@ class Compiler:
     public ExprVisitor {
 public:
     // Compiler doesn't own the root
-    explicit Compiler(ast::Expr* root):
-        ast_root{root} {
+    explicit Compiler(std::shared_ptr<ast::Expr> root):
+        ast_root{std::move(root)} {
     }
 
     void compile();
     Program get_program();
 
-    std::any visitArithmeticBinary(ast::ArithmeticBinary* binary) override;
-    std::any visitIntConstant(ast::IntConstant* constant) override;
-    std::any visitGroup(ast::Group* group) override;
+    std::any visitArithmeticBinary(std::shared_ptr<ast::ArithmeticBinary> binary) override;
+    std::any visitIntConstant(std::shared_ptr<ast::IntConstant> constant) override;
+    std::any visitGroup(std::shared_ptr<ast::Group> group) override;
 
 private:
-    ast::Expr* ast_root;
+    std::shared_ptr<ast::Expr> ast_root;
     Program prog{};
 };
 
