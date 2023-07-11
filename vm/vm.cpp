6453961be88294d756758ffffe996265cd8291f3
Gorun <307532500@qq.com>
2023-07-11 19:01:23 +0800

    实现VirtualMachine识别bool类型的结果

--- vm/vm.cpp
+++ vm/vm.cpp
@@ -83,6 +83,12 @@ void decaf::VirtualMachine::run() {
         set_int_result(stk_top);
         return;
     }
+    if (prog.result_type.classification == Type::Classification::BOOL) {
+        bool stk_top = std::any_cast<bool>(stk.top());
+        stk.pop();
+        set_bool_result(stk_top);
+        return;
+    }
 }
 
 void decaf::VirtualMachine::set_int_result(int val) {
