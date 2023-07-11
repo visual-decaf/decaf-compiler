7f6d4f2601ba5363d78796b6e6b73e8ad45c391b
Mask <shijiaxu.mask@qq.com>
2023-07-11 04:49:31 +0000

    修复Group节点content为空时的序列化错误

--- tests/ast/ast_test.cpp
+++ tests/ast/ast_test.cpp
@@ -9,7 +9,7 @@ TEST_CASE("int_constant_json", "[ast]") {
     "type": "IntConstant",
     "value": 3
 }
-)");
+    )");
     REQUIRE(expect_json == ast_root->to_json());
 }
 
@@ -40,6 +40,66 @@ TEST_CASE("arithmetic_binary_json", "[ast]") {
     "right": {
         "type": "IntConstant",
         "value": 3
+    }
+}
+    )");
+    REQUIRE(expect_json == ast_root->to_json());
+}
+
+TEST_CASE("group_json", "[ast]") {
+    auto group = std::make_shared<ast::Group>(
+        std::make_shared<ast::ArithmeticBinary>(
+            std::make_shared<ast::IntConstant>(1),
+            ast::ArithmeticBinary::Operation::PLUS,
+            std::make_shared<ast::IntConstant>(2)));
+    boost::json::value expect_json = boost::json::parse(R"(
+{
+    "type": "Group",
+    "content": {
+        "type": "ArithmeticBinary",
+        "operation": "PLUS",
+        "left": {
+            "type": "IntConstant",
+            "value": 1
+        },
+        "right": {
+            "type": "IntConstant",
+            "value": 2
+        }
+    }
+}
+    )");
+    REQUIRE(expect_json == group->to_json());
+}
+
+TEST_CASE("group_null_content_json", "[ast]") {
+    auto group = std::make_shared<ast::Group>(
+        nullptr);
+    boost::json::value expect_json = boost::json::parse(R"(
+{
+    "type": "Group",
+    "content": {}
+}
+    )");
+    REQUIRE(expect_json == group->to_json());
+}
+
+TEST_CASE("null_node_json", "[ast]") {
+    auto ast_root = std::make_shared<ast::ArithmeticBinary>(
+        std::make_shared<ast::IntConstant>(20),
+        ast::ArithmeticBinary::Operation::MOD,
+        std::make_shared<ast::Group>(nullptr));
+    boost::json::value expect_json = boost::json::parse(R"(
+{
+    "type": "ArithmeticBinary",
+    "operation": "MOD",
+    "left": {
+        "type": "IntConstant",
+        "value": 20
+    },
+    "right": {
+        "type": "Group",
+        "content": {}
     }
 }
 )");
