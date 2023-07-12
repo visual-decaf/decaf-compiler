a3083e933211eb1c0f6f85aec8e8eedb3ab38bfd
Gorun <307532500@qq.com>
2023-07-12 10:56:55 +0800

    更改虚拟机类型检测

--- vm/vm.cpp
+++ vm/vm.cpp
@@ -2,85 +2,113 @@
 #include "byte_code_driver.h"
 
 bool decaf::VirtualMachine::op_PLUS() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs + rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs + rhs, Type::Classification::INT);
     return true;
 }
 
 bool decaf::VirtualMachine::op_MINUS() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs - rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs - rhs, Type::Classification::INT);
     return true;
 }
 
 bool decaf::VirtualMachine::op_MULTIPLY() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs * rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs * rhs, Type::Classification::INT);
     return true;
 }
 
 bool decaf::VirtualMachine::op_DIVIDE() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs / rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs / rhs, Type::Classification::INT);
     return true;
 }
 
 bool decaf::VirtualMachine::op_MOD() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs % rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs % rhs, Type::Classification::INT);
     return true;
 }
 
 bool decaf::VirtualMachine::op_GET_INSTANT(uint8_t instant) {
-    stk.emplace(static_cast<int>(instant));
+    push_classification(static_cast<int>(instant), Type::Classification::INT);
     return true;
 }
 
 bool decaf::VirtualMachine::op_GET_INT_CONSTANT(uint8_t index) {
-    stk.emplace(prog.pool.get_int_constant(index));
+    push_classification(prog.pool.get_int_constant(index), Type::Classification::INT);
     return true;
 }
 
 bool decaf::VirtualMachine::op_GET_TRUE() {
-    stk.emplace(true);
+    push_classification(true, Type::Classification::BOOL);
     return true;
 }
 
 bool decaf::VirtualMachine::op_GET_FALSE() {
-    stk.emplace(false);
+    push_classification(false, Type::Classification::BOOL);
     return true;
 }
 
 bool decaf::VirtualMachine::op_LOGIC_AND() {
-    bool rhs = std::any_cast<bool>(stk.top());
-    stk.pop();
-    bool lhs = std::any_cast<bool>(stk.top());
-    stk.pop();
-    stk.emplace(lhs && rhs);
+    if (!expected_top_type_classification(Type::Classification::BOOL)) {
+        return false;
+    }
+    bool rhs = pop_as_bool();
+    if (!expected_top_type_classification(Type::Classification::BOOL)) {
+        return false;
+    }
+    bool lhs = pop_as_bool();
+    push_classification(lhs && rhs, Type::Classification::BOOL);
     return true;
 }
 
 bool decaf::VirtualMachine::op_LOGIC_OR() {
-    bool rhs = std::any_cast<bool>(stk.top());
-    stk.pop();
-    bool lhs = std::any_cast<bool>(stk.top());
-    stk.pop();
-    stk.emplace(lhs || rhs);
+    if (!expected_top_type_classification(Type::Classification::BOOL)) {
+        return false;
+    }
+    bool rhs = pop_as_bool();
+    if (!expected_top_type_classification(Type::Classification::BOOL)) {
+        return false;
+    }
+    bool lhs = pop_as_bool();
+    push_classification(lhs || rhs, Type::Classification::BOOL);
     return true;
 }
 
@@ -119,52 +147,74 @@ decaf::VirtualMachine::result_type decaf::VirtualMachine::get_result() {
 }
 
 bool decaf::VirtualMachine::op_NEGATE() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(-rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+
+    push_classification(-rhs, Type::Classification::INT);
     return true;
 }
 
 bool decaf::VirtualMachine::op_LOGIC_NOT() {
-    bool rhs = std::any_cast<bool>(stk.top());
-    stk.pop();
-    stk.emplace(!rhs);
+    if (!expected_top_type_classification(Type::Classification::BOOL)) {
+        return false;
+    }
+    bool rhs = pop_as_bool();
+
+    push_classification(!rhs, Type::Classification::BOOL);
     return true;
 }
 
 bool decaf::VirtualMachine::op_LESS() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs < rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs < rhs, Type::Classification::BOOL);
     return true;
 }
 
 bool decaf::VirtualMachine::op_LESS_EQUAL() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs <= rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs <= rhs, Type::Classification::BOOL);
     return true;
 }
 
 bool decaf::VirtualMachine::op_GREATER() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs > rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs > rhs, Type::Classification::BOOL);
     return true;
 }
 
 bool decaf::VirtualMachine::op_GREATER_EQUAL() {
-    int rhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    int lhs = std::any_cast<int>(stk.top());
-    stk.pop();
-    stk.emplace(lhs >= rhs);
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int rhs = pop_as_int();
+    if (!expected_top_type_classification(Type::Classification::INT)) {
+        return false;
+    }
+    int lhs = pop_as_int();
+    push_classification(lhs >= rhs, Type::Classification::BOOL);
     return true;
 }
 
