5282b08e92e51f5ba697cd87789dc5362234cf14
Gorun <307532500@qq.com>
2023-07-13 16:48:05 +0800

    Compiler编译语句不会在栈中留下值

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -76,7 +76,7 @@ std::any decaf::Compiler::visitArithmeticUnary(std::shared_ptr<ast::ArithmeticUn
 
 void decaf::Compiler::compile() {
     ast_root->accept(*this);
-    prog.set_result_type(ast_root->type);
+    prog.set_result_type_classification(Type::Classification::VOID);
 }
 
 decaf::Program decaf::Compiler::get_program() {
