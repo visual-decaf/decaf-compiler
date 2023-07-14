e28c5b93c2e731f3ec98a07987533b08e7c7e71e
Mask <shijiaxu.mask@qq.com>
2023-07-14 10:29:34 +0800

    为Stmt序列化提供默认实现

--- ast/stmt.h
+++ ast/stmt.h
@@ -13,9 +13,12 @@ struct StmtVisitor {
 
 namespace decaf::ast {
 
-struct Stmt {
+struct Stmt: public Serializable {
     virtual std::any accept(StmtVisitor& visitor) = 0;
     virtual bool equal(std::shared_ptr<Stmt> rhs) = 0;
+    boost::json::value to_json() override {
+        return "Statement Serializable Not Implemented";
+    }
 
     virtual ~Stmt() = default;
 };
