baa5f472fd29227f775ffc0b820865d461ddd83c
Mask <shijiaxu.mask@qq.com>
2023-07-14 13:07:04 +0800

    补充Type名称映射

--- type/type.cpp
+++ type/type.cpp
@@ -5,7 +5,7 @@ const std::map<decaf::Type::Classification, std::string> decaf::Type::type_name_
     {decaf::Type::Classification::INT, "INT"},
     {decaf::Type::Classification::FLOAT, "FLOAT"},
     {decaf::Type::Classification::BOOL, "BOOL"},
-};
+    {decaf::Type::Classification::VOID, "VOID"}};
 
 boost::json::value decaf::Type::to_json() {
     boost::json::string result{type_name_of.at(this->classification)};
