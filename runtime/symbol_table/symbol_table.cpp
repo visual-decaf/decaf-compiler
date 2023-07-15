f70334e3320bf1b989ae472867002f5841002207
Gorun <307532500@qq.com>
2023-07-15 20:43:55 +0800

    简化SymbolTable

--- runtime/symbol_table/symbol_table.cpp
+++ runtime/symbol_table/symbol_table.cpp
@@ -1,11 +1,6 @@
 #include "symbol_table.h"
 #include <utility>
 
-decaf::SymbolTable::index_type decaf::SymbolTable::add_symbol() {
-    table[count] = nullptr; // Not Available
-    return count++;
-}
-
 void decaf::SymbolTable::set_symbol(index_type index, decaf::StackItem::ptr_type ptr) {
     table[index] = std::move(ptr);
 }
--- runtime/symbol_table/symbol_table.h
+++ runtime/symbol_table/symbol_table.h
@@ -9,12 +9,10 @@ namespace decaf {
 class SymbolTable {
 public:
     using index_type = std::uint8_t;
-    index_type add_symbol();
     void set_symbol(index_type, StackItem::ptr_type);
     StackItem::ptr_type get_symbol(index_type);
 
 private:
-    int count = 0;
     std::map<index_type, StackItem::ptr_type> table;
 };
 
