d5a1f926f757f7960f5a85a6402d439e1fa7bf27
Mask <shijiaxu.mask@qq.com>
2023-07-16 15:20:32 +0800

    修改符号表序列化格式

--- tests/symbol_table/symbol_table_test.cpp
+++ tests/symbol_table/symbol_table_test.cpp
@@ -6,16 +6,22 @@ TEST_CASE("symbol_table_json", "[symbol_table]") {
     symbol_table.set_symbol(1, std::make_shared<decaf::BoolStackItem>(true));
     symbol_table.set_symbol(2, std::make_shared<decaf::IntStackItem>(1));
     auto expect_json = boost::json::parse(R"(
-{
-    "1": {
-        "type": "BOOL",
-        "value": true
+[
+    {
+        "index": 1,
+        "value": {
+            "type": "BOOL",
+            "value": true
+        }
     },
-    "2": {
-        "type": "INT",
-        "value": 1
+    {
+        "index": 2,
+        "value": {
+            "type": "INT",
+            "value": 1
+        }
     }
-}
+]
 )");
     REQUIRE(expect_json == symbol_table.to_json());
 }
