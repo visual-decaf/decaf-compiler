4cd0655703b37a33225b90036572662a0635a1be
Mask <shijiaxu.mask@qq.com>
2023-07-16 09:25:10 +0800

    新添step类表示POP、PUSH、ERROR操作

---
+++ apis/debug_vm/step/step.cpp
@@ -0,0 +1,19 @@
+#include "step.h"
+
+#include <utility>
+
+void decaf::Step::set_error_msg(std::string _error_msg) {
+    this->error_msg = std::move(_error_msg);
+}
+
+boost::json::value decaf::Step::to_json() {
+    boost::json::object result{{"operation", operation}};
+    if (operation == "POP") {
+        result["item"] = nullptr;
+    } else if (operation == "ERROR") {
+        result["item"] = error_msg;
+    } else {
+        result["item"] = this->item->to_json();
+    }
+    return result;
+}
---
+++ apis/debug_vm/step/step.h
@@ -0,0 +1,22 @@
+#include <utility>
+
+#include "stack_items.h"
+
+namespace decaf {
+
+class Step: Serializable {
+    Step(std::string _operation, std::shared_ptr<decaf::StackItem> _item):
+        operation(std::move(_operation)), item(std::move(_item)) {
+    }
+
+    void set_error_msg(std::string error_msg);
+
+    boost::json::value to_json() override;
+
+private:
+    std::string operation;
+    std::shared_ptr<decaf::StackItem> item;
+    std::string error_msg;
+};
+
+} // namespace decaf
