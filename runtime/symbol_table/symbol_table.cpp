bd13c80b59a3a3b66690481719a6a95ab0794b92
Mask <shijiaxu.mask@qq.com>
2023-07-16 08:22:31 +0800

    实现符号表序列化

--- runtime/symbol_table/symbol_table.cpp
+++ runtime/symbol_table/symbol_table.cpp
@@ -8,3 +8,10 @@ void decaf::SymbolTable::set_symbol(index_type index, decaf::StackItem::ptr_type
 decaf::StackItem::ptr_type decaf::SymbolTable::get_symbol(decaf::SymbolTable::index_type index) {
     return table[index];
 }
+boost::json::value decaf::SymbolTable::to_json() {
+    boost::json::object result;
+    for (const auto& entry: this->table) {
+        result[std::to_string(static_cast<int>(entry.first))] = entry.second->to_json();
+    }
+    return result;
+}
