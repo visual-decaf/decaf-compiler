f003641b67b0783a1eccbb5a2404226f239f06f5
Mask <shijiaxu.mask@qq.com>
2023-07-19 00:00:41 +0800

    修复VariableDecl序列化错误

--- tests/simple_driver.cpp
+++ tests/simple_driver.cpp
@@ -1,17 +1,11 @@
-#include "byte_code.h"
-#include "compiler.h"
-#include "debug_vm.h"
-#include "parser.h"
-#include "scanner.h"
 #include "visual_decaf.h"
 #include <iostream>
-#include <streambuf>
-
-using namespace decaf;
 int main() {
     int id = get_id();
-    std::string code = "int x;";
-    upload_code(code.c_str(), id);
-    std::string tokens = get_token_stream(id);
+    upload_code("int x;", id);
+    char* tokens = get_token_stream(id);
     std::cout << tokens << std::endl;
+    free(tokens);
+    char* ast = get_ast(id);
+    std::cout << ast << std::endl;
 }
